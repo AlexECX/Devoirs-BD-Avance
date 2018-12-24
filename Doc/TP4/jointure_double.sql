3adcabc871bf06cc0f0f3880597d3fbdbd420c66
AlexECX <33321263+AlexECX@users.noreply.github.com>
2018-12-24 12:09:23 -0500

    code update, ajout énoncés

--- Doc/TP4/jointure_double.sql
+++ Doc/TP4/jointure_double.sql
@@ -1,19 +1,32 @@
 
 
 -- with index
+-- drop table T1_index;
+-- drop table T2_index;
 
--- -- drop table T1_index;
--- -- drop table T2_index;
--- -- drop table T3_index;
+-- drop index indexT1;
+-- drop index indexT2;
 
--- -- drop index indexT1;
--- -- drop index indexT2;
+-- create table T1_index as select * from T1;
+-- create table T2_index as select * from T2;
 
--- -- create table T1_index as select * from T1;
--- -- create table T2_index as select * from T2;
--- -- create table T3_index as select * from T3;
+-- create index indexT1 on T1_index(col1);
+-- create index indexT2 on T2_index(col1);
 
--- -- create index indexT1 on T1_INDEX(col1);
--- -- create index indexT2 on T2_INDEX(col1);
+SELECT /*+ FULL(T3) INDEX(T1_index col1) INDEX(T2_index col1) */ COUNT(*) FROM T3,T1_index,T2_index WHERE T1_index.col1=T3.col1 AND T2_index.col1 = T3.col2 AND T1_index.col2 = 1;
 
-SELECT /*+ FULL)*/ COUNT(*) FROM T1,T2,T3 WHERE T1.col1=T3.col1 AND T2.col1 = T3.col2 AND T1.col2 = 1
+-- with cluster
+
+-- drop cluster clusterT1;
+-- drop cluster clusterT2;
+
+-- create cluster clusterT1(var1 INTEGER)
+-- SIZE 160 HASHKEYS 100000;
+
+-- create cluster clusterT2(var1 INTEGER)
+-- SIZE 160 HASHKEYS 100000;
+
+-- create table T1_CLUSTER cluster clusterT1(col1) as select * from T1;
+-- create table T2_CLUSTER cluster clusterT2(col1) as select * from T2;
+
+-- SELECT /*+ INDEX(T1_index col1) INDEX(T2_index col1)*/ COUNT(*) FROM T3,T1_index,T2_index WHERE T1_index.col1=T3.col1 AND T2_index.col1 = T3.col2 AND T1_index.col2 = 1
--- Doc/TP4/workspace.sql
+++ Doc/TP4/workspace.sql
@@ -8,26 +8,25 @@
 -- create index indexT1 on T1_INDEX(col1);
 -- create index indexT2 on T2_INDEX(col1);
 
--- select /*+ FULL*/ count(*) from T1, T2 where T1.col1 = T2.col1;
--- select /*+ USE_NL_WITH_INDEX(T1_INDEX col1) USE_NL_WITH_INDEX(T2_INDEX col1)*/ count(*) from T1_INDEX, T2_INDEX where T1_INDEX.col1 = T2_INDEX.col1;
+select /*+ USE_NL_WITH_INDEX(T1_INDEX T2_INDEX) */ count(*) from T1_INDEX, T2_INDEX where T1_INDEX.col1 = T2_INDEX.col1;
 
 -----------
 
-drop cluster clusterT1;
-drop cluster clusterT2;
+-- drop cluster clusterT1;
+-- drop cluster clusterT2;
 
-create cluster clusterT1(var1 INTEGER)
-SIZE 250 INDEX;
+-- create cluster clusterT1(var1 INTEGER)
+-- SIZE 250 INDEX;
 
-create INDEX indexclusterT1 on cluster clusterT1;
+-- create INDEX indexclusterT1 on cluster clusterT1;
 
-create cluster clusterT2(var1 INTEGER)
-SIZE 250 INDEX;
+-- create cluster clusterT2(var1 INTEGER)
+-- SIZE 250 INDEX;
 
-create INDEX indexclusterT2 on cluster clusterT2;
+-- create INDEX indexclusterT2 on cluster clusterT2;
 
-create table T1_CLUSTER cluster clusterT1(col1) as select * from T1;
-create table T2_CLUSTER cluster clusterT2(col1) as select * from T2;
+-- create table T1_CLUSTER cluster clusterT1(col1) as select * from T1;
+-- create table T2_CLUSTER cluster clusterT2(col1) as select * from T2;
 
-select count(*) from T1_CLUSTER, T2_CLUSTER where T1_CLUSTER.col1 = T2_CLUSTER.col1;
+-- select count(*) from T1_CLUSTER, T2_CLUSTER where T1_CLUSTER.col1 = T2_CLUSTER.col1;
 
